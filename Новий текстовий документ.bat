9e23105918c8cc2950f38ef5ebf81320fb5fcb1b
Dmytro <myemail@gmail.com>
2025-03-10 23:40:25 +0200

    Перша версія

--- Новий текстовий документ.bat
+++ Новий текстовий документ.bat
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "Перша версія"
 git branch -M main
-git remote add origin git remote add origin https://github.com/popey123123/personal-account.git
+git remote add origin git remote add origin https://github.com/popey123123/loveplay
 
 git push -u origin main
